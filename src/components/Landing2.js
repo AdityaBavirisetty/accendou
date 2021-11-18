9c02b6758d8f286fe6386057d8820a6e71fddfbf
AdityaBavirisetty <adityabavirisetty65@gmail.com>
2021-11-18 19:59:32 +0530

    no changes

--- src/components/Landing2.js
+++ src/components/Landing2.js
@@ -8,6 +8,7 @@ const Landing2 = () => {
                       <div className="box" >
                                 <Link to="/current" style={{textDecoration: "none"}}><p>Customer</p></Link>
                       </div>
+                      
                       <div className="box">
                                 {/* <Link to="/insurer" style={{textDecoration: "none"}}><p>Insurer</p></Link> */}
                                 <a href="http://15.207.104.100/home.html" target="_blank" style={{textDecoration:'none'}}><p>Insurer</p></a>
